017a241d80faf09c36740369b98f90b033adbdcd
harith-a <harithamaran@gmail.com>
2025-02-22 16:38:53 +0800

    add github actions

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: 'https://harith.my/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
